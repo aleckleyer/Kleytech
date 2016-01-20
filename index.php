cd3846e0f0a47a1598e5e4b629d47f0b0ab805dd
Alec Kleyer <alec@kleytech.com>
2016-01-20 11:40:23 -0500

    Initial Commits

---
+++ index.php
@@ -0,0 +1,294 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>KleyTech</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    <link href="main.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <nav id="header" class="navbar navbar-default navbar-fixed-top">
+    	<div id="header-container" class="container navbar-container">
+        	<div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a id="brand" class="navbar-brand" href="#">KleyTech</a>
+			</div>
+			<div id="navbar" class="collapse navbar-collapse">
+				<ul class="nav navbar-nav">
+					<li class="active"><a href="#">Home</a></li>
+					<li><a href="#about">About Me</a></li>
+					<li><a href="#experience">Experience</a></li>
+					<li><a href="#portfolio">Portfolio</a></li>
+					<li><a href="#activities">Interests</a></li>
+					<li><a href="#contact">Contact</a></li>
+				</ul>
+			</div><!-- /.nav-collapse -->
+		</div><!-- /.container -->
+	</nav><!-- /.navbar -->
+	<header id="page-top">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<img class="img-responsive" src="LinkedInProfilePic.jpg" id="profilePic" alt="">
+					<div class="intro-text">
+						<span class="name">Hello, my name is Alec Kleyer</span>
+						<hr class="star-light">
+						<p class="skills"><i class="fa fa-quote-left"></i>&nbsp;Programming is one of the most difficult branches of applied mathematics; <br/>the poorer mathematicians had better remain pure mathematicians&nbsp;<i class="fa fa-quote-right"></i></p>
+						<div class="col-md-12">
+							<div class="col-md-offset-7 col-md-2">
+								<p class="skills"> - Edsger Dijkstra</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
+	<div class="content-wrapper">
+    <section class="success" id="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2>About</h2>
+                    <hr class="star-light">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 col-lg-offset-2">
+                    <h3>Education</h3>
+                    <hr>
+                    <h4><i>Macaulay Honors College at the College of Staten Island, The City University of New York</i></h4>
+                    <p>B.S. in Computer Science – Mathematics</p>
+                    <p>Major GPA: 3.72/4.00</p>
+                </div>
+                <div class="col-lg-4">
+                	<h3>Research</h3>
+                	<hr>
+                	<h4><i>Game Theory</i></h4>
+                	<p>Worked directly with my Mathematics Professor Kevin O’Bryant to develop an algorithm in Wolfram Mathematica to analyze and simplify combinatorial games.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="primary" id="experience">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2>Experience</h2>
+                    <hr class="star-primary" id="portfolioStar">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-6 col-lg-4">
+					<div class="box">							
+						<div class="icon">
+							<div class="image"><i class="fa fa-thumbs-o-up"></i></div>
+							<div class="info">
+								<h3 class="title">BigBentoBox</h3>
+								<ul>
+									<li><p>Worked directly with the Manager of the Development Department to assist the production of their product’s web portal using Ruby on Rails and GIT version control.</p></li>
+									<li><p>Assisted the Chief Web Developer in updating the company’s website while furthering my knowledge and experience in PHP and front-end web design programming languages.</p></li>
+									<li><p>Worked directly with the Manager of the Financial Department to produce competitive market surveys and develop research analyses using Microsoft Excel in order to increase my managers’ understanding of the industry in other countries.</p></li>
+								</ul>
+							</div>
+						</div>
+						<div class="space"></div>
+					</div> 
+				</div>
+				<div class="col-xs-12 col-sm-6 col-lg-4">
+					<div class="box">							
+						<div class="icon">
+							<div class="image"><i class="fa fa-flag"></i></div>
+							<div class="info">
+								<h3 class="title">PNG Computers</h3>
+								<ul>
+									<li><p>Disassembled HP, Dell, Apple, Compaq, and Acer computers and laptops to analyze and fix issues for customers ranging from individuals to big corporations.</p></li>
+									<li><p>Promoted the business on EBay using Microsoft Access and Excel to catalog and print queries in order to expand the store’s sales.</p></li>
+									<li><p>Performed tune-ups and assisted customers with simplifying their computer usage in order to increase customer service and communication skills.</p></li>
+								</ul>
+							</div>
+						</div>
+						<div class="space"></div>
+					</div> 
+				</div>
+				<div class="col-xs-12 col-sm-6 col-lg-4">
+					<div class="box">							
+						<div class="icon">
+							<div class="image"><i class="fa fa-desktop"></i></div>
+							<div class="info">
+								<h3 class="title">Excelidea</h3>
+								<ul>
+									<li><p>Reformatted text and advertisements using HTML and CSS in order to increase conversion rates on Google AdWords and AdSense and to increase the website’s organic page ranking in search engines.</p></li>
+									<li><p>Assisted in managing customer information using a SQL database to keep the business running smoothly and allow less time to search for necessary customer details.</p></li>
+									<li><p>Managed the website using an online web server, Eco Web Hosting, and private repository, BitBucket, using GIT Version Control to push files to BitBucket and further upload them to the web server using an FTP Client, FireFTP.</p></li>
+								</ul>
+							</div>
+						</div>
+						<div class="space"></div>
+					</div> 
+				</div>		    
+            </div>
+        </div>
+    </section>
+    <section class="success" id="portfolio">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2>Portfolio</h2>
+                    <hr class="star-light" id="portfolioStar">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-offset-4 col-sm-4 col-sm-offset-4">
+                    <div class="thumbnail">
+               			<div class="caption">
+                    		<h4>IRentTLC</h4>
+                    			<p>Taxi/Livery Car Rental</p>
+                    			<p>	<a href="http://irenttlc.com" class="label label-danger" target="_blank" rel="tooltip" title="Visit">Visit Page</a></p>
+               		 	</div>
+                		<img src="IRentTLC.png" alt="...">
+            		</div>    
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="primary" id="activities">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2>Activities/Interests</h2>
+                    <hr class="star-primary" id="portfolioStar">
+                </div>
+            </div>
+             <div class="row">
+                <div class="col-xs-12 col-sm-6 col-lg-offset-3 col-lg-6">
+					<div class="box">							
+						<div class="icon">
+							<div class="image"><i class="fa fa-thumbs-o-up"></i></div>
+							<div class="info">
+								<h3 class="title">Kappa Sigma</h3>
+								<ul>
+									<li><p>Met with Regional Expansion Managers of Kappa Sigma National Fraternity in order to promote and seek assistance for founding a chapter on the College of Staten Island’s campus.</p></li>
+									<li><p>Promoted the newly founded chapter in our first year through developing a mobile application that would help organize the chapter and promote communications through its members.</p></li>
+								</ul>
+							</div>
+						</div>
+						<div class="space"></div>
+					</div> 
+				</div>
+            </div>
+        </div>
+    </section>
+    <section class="success" id="contact">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2>Contact</h2>
+                    <hr class="star-light">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2">
+                    <!-- To configure the contact form email address, go to mail/contact_me.php and update the email address in the PHP file on line 19. -->
+                    <!-- The form should work on most web servers, but if the form is not working you may need to configure your web server differently. -->
+                    <form name="sentMessage" id="contactForm" novalidate="">
+                        <div class="row control-group">
+                            <div class="form-group col-xs-12 floating-label-form-group controls">
+                                <label>Name</label>
+                                <input type="text" class="form-control" placeholder="Name" id="name" required="" data-validation-required-message="Please enter your name.">
+                                <p class="help-block text-danger"></p>
+                            </div>
+                        </div>
+                        <div class="row control-group">
+                            <div class="form-group col-xs-12 floating-label-form-group controls">
+                                <label>Email Address</label>
+                                <input type="email" class="form-control" placeholder="Email Address" id="email" required="" data-validation-required-message="Please enter your email address.">
+                                <p class="help-block text-danger"></p>
+                            </div>
+                        </div>
+                        <div class="row control-group">
+                            <div class="form-group col-xs-12 floating-label-form-group controls">
+                                <label>Phone Number</label>
+                                <input type="tel" class="form-control" placeholder="Phone Number" id="phone" required="" data-validation-required-message="Please enter your phone number.">
+                                <p class="help-block text-danger"></p>
+                            </div>
+                        </div>
+                        <div class="row control-group">
+                            <div class="form-group col-xs-12 floating-label-form-group controls">
+                                <label>Message</label>
+                                <textarea rows="5" class="form-control" placeholder="Message" id="message" required="" data-validation-required-message="Please enter a message."></textarea>
+                                <p class="help-block text-danger"></p>
+                            </div>
+                        </div>
+                        <br>
+                        <div id="success"></div>
+                        <div class="row">
+                            <div class="form-group col-xs-12">
+                                <button type="submit" class="btn btn-default btn-lg" id="contactSubmit">Send</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+    <footer class="container">
+    	<div class="row">
+    		<div class="col-md-3">
+    		</div>
+    		<div class="col-md-6">
+    			<ul class="list list-inline">
+    				<li><a href="http://www.facebook.com/akleyer" target="_blank"><i class="fa fa-facebook-square"></i></a></li>
+    				<li><a href="https://twitter.com/kleytech" target="_blank"><i class="fa fa-twitter-square"></i></a></li>
+    				<li><a href="https://www.linkedin.com/pub/alec-kleyer/a5/656/650" target="_blank"><i class="fa fa-linkedin-square"></i></a></li>
+    				<li><a href="https://plus.google.com/101918177218380195746/videos" target="_blank"><i class="fa fa-google-plus-square"></i></a></li>
+    			</ul>
+    		</div>
+    		<div class="col-md-3">
+    		</div>
+    	</div>
+    	<div class="row">
+    		<div class="col-md-3">
+    		</div>
+    		<div class="col-md-6">
+    		</div>
+    		<div class="col-md-3">
+    		</div>
+    	</div>
+    </footer>
+	</div>
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-46803231-4', 'auto');
+	  ga('send', 'pageview');
+
+	</script>
+	<script src="scripts.js"></script>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
